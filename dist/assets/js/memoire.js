b05aeeb37b20f3efef9486a80daa56a9554acb84
thomas.djekic@student.hepl.be <thomas.djekic@student.hepl.be>
2023-12-18 15:21:49 +0100

    :bug: Fixing bug

--- dist/assets/js/memoire.js
+++ dist/assets/js/memoire.js
@@ -4,10 +4,10 @@
     OUTPUT : Retourne une couleur
 */
 
-function getRandomColor ()
+/*eslint-disable-next-line no-unused-vars*/
+function getRandomColor()
 {
     const arrayColor = ["violet", "indigo", "blue", "cyan", "green", "yellow", "orange", "red"];
     const indexColor = Math.floor(Math.random() * 8);
-    
     return arrayColor[indexColor];
 }
